5434ffe3fae450d65dc6ca82832d37556ee2bc24
Rahul <gupta-9631>
2022-10-14 23:22:40 +0530

    made changes

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
+
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
